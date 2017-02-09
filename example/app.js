8f6870ff2a5a6f433bfab35f709a552c0e9876cb
Francois Laubscher <francoislaubscher@outlook.com>
2017-02-09 13:39:30 +0200

    reverted example

--- example/app.js
+++ example/app.js
@@ -1,47 +1,39 @@
-const express = require('express');
-const nodeExcel = require('../index');
-const app = express();
+var express = require('express');
+var nodeExcel = require('../index');
+var app = express();
 
 app.get('/', function (req, res) {
-  const conf = {
+  var conf = {
     stylesXmlFile: __dirname + '/styles.xml',
     name: 'report',
     cols: [
       {
-        caption: '',
+        caption: 'First Name',
         type: 'string',
         width: 15
       },
       {
-        caption: '',
+        caption: 'Last Name',
         type: 'string',
         width: 15
       },
       {
-        caption: '',
+        caption: 'Email',
         type: 'string',
         width: 30
       }
     ],
     rows: [
-      [{ value: '', style: 1 }, { value: '', style: 1 }, { value: '', style: 1 }],
-      [{ value: '', style: 1 }, { value: 'THIS IS THE REPORT NAME', style: 1 }, { value: '', style: 1 }],
-      [{ value: '', style: 1 }, { value: '', style: 1 }, { value: '', style: 1 }],
-      ['Report Generated: 2017-02-08', '', ''],
-      ['Event: Dat event do', '', ''],
-      ['By: Francois Laubscher', '', ''],
-      ['', '', ''],
-      [{ value: 'First Name', style: 1 }, { value: 'Last Name', style: 1 }, { value: 'Email', style: 1 }],
       ['Bruce', 'Wayne', 'info@bat.man'],
       ['Clark', 'Kent', 'info@super.man'],
       ['Peter', 'Parker', 'info@spider.man']
     ]
   };
 
-  const result = nodeExcel.execute(conf);
+  var result = nodeExcel.execute(conf);
   res.setHeader('Content-Type', 'application/vnd.openxmlformats');
   res.setHeader('Content-Disposition', 'attachment; filename=Report.xlsx');
-  return res.send(new Buffer(result, 'binary'));
+  res.send(new Buffer(result, 'binary'));
 });
 
 app.listen(3001, function () {
